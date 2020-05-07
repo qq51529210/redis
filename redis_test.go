c4b8699cae82c3477dd25b4be513a11dd84ca25e
ben <51529210@qq.com>
2020-05-07 15:02:33 +0800

    u

--- redis_test.go
+++ redis_test.go
@@ -3,8 +3,10 @@ package redis
 import (
 	"bytes"
 	"encoding/json"
+	"net"
 	"strconv"
 	"testing"
+	"time"
 )
 
 func TestCmd_SimpleString(t *testing.T) {
@@ -352,6 +354,45 @@ func TestCmd_Value(t *testing.T) {
 			t.FailNow()
 		}
 	}
+}
+
+func TestClient(t *testing.T) {
+	client := NewClient(func() (net.Conn, error) {
+		return net.Dial("tcp", "192.168.1.30:6379")
+	}, 10, time.Second, time.Second)
+	defer client.Close()
+	//
+	cmd := client.NewCmd("set")
+	cmd.String("a")
+	cmd.Int(1)
+	val, err := client.DoCmd(cmd)
+	if err != nil {
+		t.Fatal(err)
+	}
+	switch val.(type) {
+	case string:
+		if val.(string) != "OK" {
+			t.FailNow()
+		}
+	default:
+		t.FailNow()
+	}
+	//
+	cmd = client.NewCmd("get")
+	cmd.String("a")
+	val, err = client.DoCmd(cmd)
+	if err != nil {
+		t.Fatal(err)
+	}
+	switch val.(type) {
+	case string:
+		if val.(string) != "1" {
+			t.FailNow()
+		}
+	default:
+		t.FailNow()
+	}
+	t.Log(val)
 }
 
 func BenchmarkCmd_SimpleString(b *testing.B) {
