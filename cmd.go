91568e9eb871540cac44a16bb721802d10899626
ben <51529210@qq.com>
2020-09-07 22:12:01 +0800

    u

--- cmd.go
+++ cmd.go
@@ -2,7 +2,6 @@ package redis
 
 import (
 	"encoding/json"
-	"errors"
 	"fmt"
 	"io"
 	"strconv"
@@ -35,93 +34,93 @@ func putCmd(cmd *Cmd) {
 type Cmd struct {
 	buf [256]byte // 读取响应数据/格式化的缓存
 	req []byte    // 请求数据
-	cot int       // 当前命令的个数
+	cmd int       // 当前命令的个数
 	res []byte    // 未解析的响应数据
 	idx int       // 未解析的响应数据开始下标
 	fmt []byte    // 格式化数组的缓存，减少内存分配
 }
 
 // 重置请求缓存，除非自己保存，所有Client.NewCmd()自动调用了
-func (this *Cmd) Reset() {
-	this.req = this.req[:0]
-	this.cot = 0
+func (c *Cmd) Reset() {
+	c.req = c.req[:0]
+	c.cmd = 0
 }
 
 // 编码简单字符串
-func (this *Cmd) SimpleString(s string) {
-	this.cot++
+func (c *Cmd) SimpleString(s string) {
+	c.cmd++
 	// '+'
-	this.req = append(this.req, '+')
+	c.req = append(c.req, '+')
 	// s
-	this.req = append(this.req, s...)
+	c.req = append(c.req, s...)
 	// \r\n
-	this.req = append(this.req, endLine...)
+	c.req = append(c.req, endLine...)
 }
 
 // 编码错误
-func (this *Cmd) Error(s string) {
-	this.cot++
+func (c *Cmd) Error(s string) {
+	c.cmd++
 	// '-'
-	this.req = append(this.req, '-')
+	c.req = append(c.req, '-')
 	// s
-	this.req = append(this.req, s...)
+	c.req = append(c.req, s...)
 	// \r\n
-	this.req = append(this.req, endLine...)
+	c.req = append(c.req, endLine...)
 }
 
 // 格式化整数，作为字符串处理，否则服务端会报错
 // 定义这个':'协议，但不让客户端用，很奇怪
-func (this *Cmd) Int(n int64) {
-	this.fmt = this.fmt[:0]
-	this.fmt = strconv.AppendInt(this.fmt, n, 10)
-	this.Bytes(this.fmt)
-	//this.cot++
+func (c *Cmd) Int(n int64) {
+	c.fmt = c.fmt[:0]
+	c.fmt = strconv.AppendInt(c.fmt, n, 10)
+	c.Bytes(c.fmt)
+	//c.cmd++
 	//// ':'
-	//this.req = append(this.req, ':')
+	//c.req = append(c.req, ':')
 	//// n
-	//this.req = strconv.AppendInt(this.req, n, 10)
+	//c.req = strconv.AppendInt(c.req, n, 10)
 	//// \r\n
-	//this.req = append(this.req, endLine...)
+	//c.req = append(c.req, endLine...)
 }
 
 // 编码字符串
-func (this *Cmd) String(s string) {
-	this.cot++
+func (c *Cmd) String(s string) {
+	c.cmd++
 	// '$'
-	this.req = append(this.req, '$')
+	c.req = append(c.req, '$')
 	// length
-	this.req = strconv.AppendInt(this.req, int64(len(s)), 10)
+	c.req = strconv.AppendInt(c.req, int64(len(s)), 10)
 	// \r\n
-	this.req = append(this.req, endLine...)
+	c.req = append(c.req, endLine...)
 	// b
-	this.req = append(this.req, s...)
+	c.req = append(c.req, s...)
 	// \r\n
-	this.req = append(this.req, endLine...)
+	c.req = append(c.req, endLine...)
 }
 
 // 编码二进制数组，作为字符串处理
-func (this *Cmd) Bytes(b []byte) {
-	this.cot++
+func (c *Cmd) Bytes(b []byte) {
+	c.cmd++
 	// '$'
-	this.req = append(this.req, '$')
+	c.req = append(c.req, '$')
 	// length
-	this.req = strconv.AppendInt(this.req, int64(len(b)), 10)
+	c.req = strconv.AppendInt(c.req, int64(len(b)), 10)
 	// \r\n
-	this.req = append(this.req, endLine...)
+	c.req = append(c.req, endLine...)
 	// b
-	this.req = append(this.req, b...)
+	c.req = append(c.req, b...)
 	// \r\n
-	this.req = append(this.req, endLine...)
+	c.req = append(c.req, endLine...)
 }
 
 // 格式化浮点数，作为字符串处理
-func (this *Cmd) Float(n float64) {
-	this.fmt = this.fmt[:0]
-	this.fmt = strconv.AppendFloat(this.fmt, n, 'f', -1, 64)
-	this.Bytes(this.fmt)
-	//this.cot++
+func (c *Cmd) Float(n float64) {
+	c.fmt = c.fmt[:0]
+	c.fmt = strconv.AppendFloat(c.fmt, n, 'f', -1, 64)
+	c.Bytes(c.fmt)
+	//c.cmd++
 	//// '$'
-	//this.req = append(this.req, '$')
+	//c.req = append(c.req, '$')
 	//strconv.app
 }
 
@@ -132,61 +131,61 @@ func (this *Cmd) Float(n float64) {
 // case Floatxx: Float()
 // case []byte: Bytes()
 // default: Json
-func (this *Cmd) Value(v interface{}) error {
-	this.cot++
-	return this.value(v)
+func (c *Cmd) Value(v interface{}) error {
+	c.cmd++
+	return c.value(v)
 }
 
-func (this *Cmd) value(v interface{}) (err error) {
+func (c *Cmd) value(v interface{}) (err error) {
 	switch v.(type) {
 	case int8:
-		this.Int(int64(v.(int8)))
+		c.Int(int64(v.(int8)))
 	case uint8:
-		this.Int(int64(v.(uint8)))
+		c.Int(int64(v.(uint8)))
 	case int16:
-		this.Int(int64(v.(int16)))
+		c.Int(int64(v.(int16)))
 	case uint16:
-		this.Int(int64(v.(uint16)))
+		c.Int(int64(v.(uint16)))
 	case int32:
-		this.Int(int64(v.(int32)))
+		c.Int(int64(v.(int32)))
 	case uint32:
-		this.Int(int64(v.(uint32)))
+		c.Int(int64(v.(uint32)))
 	case int64:
-		this.Int(v.(int64))
+		c.Int(v.(int64))
 	case uint64:
-		this.Int(int64(v.(uint64)))
+		c.Int(int64(v.(uint64)))
 	case int:
-		this.Int(int64(v.(int)))
+		c.Int(int64(v.(int)))
 	case uint:
-		this.Int(int64(v.(uint)))
+		c.Int(int64(v.(uint)))
 	case float32:
-		this.Float(float64(v.(float32)))
+		c.Float(float64(v.(float32)))
 	case float64:
-		this.Float(v.(float64))
+		c.Float(v.(float64))
 	case string:
-		this.String(v.(string))
+		c.String(v.(string))
 	case []byte:
-		this.Bytes(v.([]byte))
+		c.Bytes(v.([]byte))
 	case []interface{}:
-		err = this.Array(v.([]interface{}))
+		err = c.Array(v.([]interface{}))
 	default:
-		err = this.Json(v)
+		err = c.Json(v)
 	}
 	return
 }
 
 // 编码数组
-func (this *Cmd) Array(a []interface{}) (err error) {
-	this.cot++
+func (c *Cmd) Array(a []interface{}) (err error) {
+	c.cmd++
 	// '*'
-	this.req = append(this.req, '*')
+	c.req = append(c.req, '*')
 	// count
-	this.req = strconv.AppendInt(this.req, int64(len(a)), 10)
+	c.req = strconv.AppendInt(c.req, int64(len(a)), 10)
 	// \r\n
-	this.req = append(this.req, endLine...)
+	c.req = append(c.req, endLine...)
 	// item
 	for i := 0; i < len(a); i++ {
-		err = this.value(a[i])
+		err = c.value(a[i])
 		if err != nil {
 			break
 		}
@@ -195,60 +194,61 @@ func (this *Cmd) Array(a []interface{}) (err error) {
 }
 
 // 格式化json字符串，作为字符串处理
-func (this *Cmd) Json(v interface{}) error {
+func (c *Cmd) Json(v interface{}) error {
 	data, err := json.Marshal(v)
 	if err != nil {
 		return err
 	}
-	this.Bytes(data)
+	c.Bytes(data)
 	return nil
 }
 
 // 返回格式化的请求缓存
-func (this *Cmd) Cmd() []byte {
-	return this.req
+func (c *Cmd) Cmd() []byte {
+	return c.req
 }
 
-func (this *Cmd) WriteTo(writer io.Writer) (int64, error) {
-	this.fmt = this.fmt[:0]
+func (c *Cmd) WriteTo(writer io.Writer) (int64, error) {
+	c.fmt = c.fmt[:0]
 	// '*'
-	this.fmt = append(this.fmt, '*')
+	c.fmt = append(c.fmt, '*')
 	// count
-	this.fmt = strconv.AppendInt(this.fmt, int64(this.cot), 10)
+	c.fmt = strconv.AppendInt(c.fmt, int64(c.cmd), 10)
 	// \r\n
-	this.fmt = append(this.fmt, endLine...)
+	c.fmt = append(c.fmt, endLine...)
 	// 先写命令单词的个数
-	n, err := writer.Write(this.fmt)
+	n, err := writer.Write(c.fmt)
 	if err != nil {
 		return 0, err
 	}
 	// 在写命令
-	m, err := writer.Write(this.req)
+	m, err := writer.Write(c.req)
 	return int64(n + m), err
 }
 
 // 读取一个数据
-func (this *Cmd) ReadValue(reader io.Reader) (interface{}, error) {
+func (c *Cmd) ReadValue(reader io.Reader) (interface{}, error) {
 	// 先读一行
-	line, err := this.readLine(reader)
+	line, err := c.readLine(reader)
 	if err != nil {
 		return nil, err
 	}
 	// 判断数据类型
 	switch line[0] {
-	case '+': // 简单字符串
+	case '+', '-': // 简单字符串，或者错误
 		return string(line[1 : len(line)-2]), nil
-	case '-': // 错误
-		return errors.New(string(line[1 : len(line)-2])), nil
 	case ':': // 整数
 		return parseInt(line[1 : len(line)-2])
 	case '$': // 字符串
-		length, err := parseInt(line[1 : len(line)-2])
+		n, err := parseInt(line[1 : len(line)-2])
 		if err != nil {
 			return nil, err
 		}
+		if n < 1 {
+			return "", nil
+		}
 		// 再读n+2个字节
-		line, err = this.readN(reader, length+2)
+		line, err = c.readN(reader, n+2)
 		if err != nil {
 			return nil, err
 		}
@@ -260,7 +260,7 @@ func (this *Cmd) ReadValue(reader io.Reader) (interface{}, error) {
 		}
 		var values []interface{}
 		for i := 0; i < int(count); i++ {
-			value, err := this.ReadValue(reader)
+			value, err := c.ReadValue(reader)
 			if err != nil {
 				return nil, err
 			}
@@ -268,105 +268,105 @@ func (this *Cmd) ReadValue(reader io.Reader) (interface{}, error) {
 		}
 		return values, nil
 	default:
-		return nil, fmt.Errorf("invalid data type %d from server", this.buf[0])
+		return nil, fmt.Errorf("invalid data type %d from server", c.buf[0])
 	}
 }
 
 // 从reader，或者this.res中，读取1行...\r\n
-func (this *Cmd) readLine(reader io.Reader) ([]byte, error) {
+func (c *Cmd) readLine(reader io.Reader) ([]byte, error) {
 	var n, i int
 	var err error
 	// 是否有未处理的数据
-	if len(this.res) > 0 {
-		if len(this.res) == this.idx {
+	if len(c.res) > 0 {
+		if len(c.res) == c.idx {
 			// 没有未处理的数据，重置
-			this.res = this.res[:0]
-			this.idx = 0
+			c.res = c.res[:0]
+			c.idx = 0
 		} else {
 			// 有未处理的数据，检查有没有完整的一行
-			i = indexEndLine(this.res[this.idx:])
+			i = indexEndLine(c.res[c.idx:])
 			if i >= 0 {
 				// 有完整的一行
-				n = this.idx
-				this.idx += i
-				return this.res[n:this.idx], nil
+				n = c.idx
+				c.idx += i
+				return c.res[n:c.idx], nil
 			}
 		}
 	}
 	// 开始
 	for {
 		// 读数据
-		n, err = reader.Read(this.buf[:])
+		n, err = reader.Read(c.buf[:])
 		if err != nil {
 			return nil, err
 		}
-		i = indexEndLine(this.buf[:n])
+		i = indexEndLine(c.buf[:n])
 		if i >= 0 {
-			if this.idx > 0 {
+			if c.idx > 0 {
 				// 接着上一次没有读完的数据
-				this.res = append(this.res, this.buf[:n]...)
-				this.idx += i
-				return this.res[:this.idx], nil
+				c.res = append(c.res, c.buf[:n]...)
+				c.idx += i
+				return c.res[:c.idx], nil
 			}
 			// 不拷贝到res
 			if n > i {
 				// 有多余的数据
-				this.res = append(this.res, this.buf[i:n]...)
+				c.res = append(c.res, c.buf[i:n]...)
 			}
 			// 返回，带\r\n
-			return this.buf[:i], nil
+			return c.buf[:i], nil
 		}
 		// 没有，继续
-		this.res = append(this.res, this.buf[:n]...)
-		this.idx = len(this.res)
+		c.res = append(c.res, c.buf[:n]...)
+		c.idx = len(c.res)
 	}
 }
 
 // 从reader，或者this.res中，读取n个字节
-func (this *Cmd) readN(reader io.Reader, m int64) ([]byte, error) {
+func (c *Cmd) readN(reader io.Reader, m int64) ([]byte, error) {
 	var n int
 	var err error
 	// 是否有未处理的数据
-	if len(this.res) > 0 {
-		if len(this.res) == this.idx {
+	if len(c.res) > 0 {
+		if len(c.res) == c.idx {
 			// 没有未处理的数据，重置
-			this.res = this.res[:0]
-			this.idx = 0
+			c.res = c.res[:0]
+			c.idx = 0
 		} else {
 			// 有未处理的数据，检查是否够m个字节
-			if int64(len(this.res[this.idx:])) >= m {
+			if int64(len(c.res[c.idx:])) >= m {
 				// 有m个字节
-				n := this.idx
-				this.idx += int(m)
-				return this.res[n:this.idx], nil
+				n := c.idx
+				c.idx += int(m)
+				return c.res[n:c.idx], nil
 			}
 		}
 	}
 	// 开始
 	for {
 		// 读数据
-		n, err = reader.Read(this.buf[:])
+		n, err = reader.Read(c.buf[:])
 		if err != nil {
 			return nil, err
 		}
-		if this.idx > 0 {
+		if c.idx > 0 {
 			// 接着上一次没有读完的数据
-			this.res = append(this.res, this.buf[:n]...)
-			if int64(len(this.res[this.idx:])) >= m {
+			c.res = append(c.res, c.buf[:n]...)
+			if int64(len(c.res[c.idx:])) >= m {
 				// 有m个字节
-				n := this.idx
-				this.idx += int(m)
-				return this.res[n:this.idx], nil
+				n := c.idx
+				c.idx += int(m)
+				return c.res[n:c.idx], nil
 			}
 		} else {
 			// 上一次没有数据
 			if int64(n) == m {
 				// 正好
-				return this.buf[:n], nil
+				return c.buf[:n], nil
 			} else if int64(n) > m {
 				// 多余的数据，保存
-				this.res = append(this.res, this.buf[m:n]...)
-				return this.buf[:m], nil
+				c.res = append(c.res, c.buf[m:n]...)
+				return c.buf[:m], nil
 			}
 		}
 		// 不够，继续
